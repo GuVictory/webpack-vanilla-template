6c0b7a11a4ad2a0dbf1afdf0750503930369ffe1
GuVictory <gubanova.vi10@gmail.com>
2023-03-09 19:43:20 +0300

    initial setup

---
+++ _config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: 'Webpack Vanilla JS — Starter Template',
+    description:
+        'Start a Vanilla JavaScript App, SPA, landing page or website using Webpack, Babel, Core-JS, SASS / SCSS.',
+    keywords:
+        'javascript, js, vanilla-javascript, vanilla-js, template, boilerplate, webpack, babel, sass, scss, css, github',
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const app = express();
+const config = require('./webpack/webpack.config.js');
+const compiler = webpack(config);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(
+    webpackDevMiddleware(compiler, {
+        publicPath: config.output.publicPath,
+    })
+);
+
+app.listen(PORT, function () {
+    console.log(`Webpack app listening on port ${PORT}!\n`);
+});
